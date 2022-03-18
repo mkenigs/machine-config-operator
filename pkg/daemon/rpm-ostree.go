5d687c58df83f5959d1d9d52ba3adf50a4ed52e7
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2022-03-18 14:43:05 -0500

    rpm-ostree.go: add Cat()

--- pkg/daemon/rpm-ostree.go
+++ pkg/daemon/rpm-ostree.go
@@ -3,8 +3,10 @@ package daemon
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -423,6 +425,10 @@ func (r *RpmOstreeClient) RebaseLayered(imgURL string) (changed bool, err error)
 
 	changed = true
 	return
+}
+
+func Cat(stateroot, commit, path string) ([]byte, error) {
+	return ioutil.ReadFile(filepath.Join(fmt.Sprintf("/ostree/deploy/%s/deploy/%s.0", stateroot, commit), path))
 }
 
 // truncate a string using runes/codepoints as limits.
